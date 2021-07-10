4aa8d11ac0a21f5959cbe2c5fe99dc3868ba5043
Aniket kumar <ani1998ket@gmail.com>
2021-07-10 14:51:36 +0530

    Adds GameObject class

---
+++ src/GameObject.js
@@ -0,0 +1,31 @@
+import {Vector2} from "./Math.js"
+
+export default class GameObject{
+    constructor(){
+        this.position = new Vector2();
+        this.velocity = new Vector2();
+        this.acceleration = new Vector2();
+    }
+
+    setPosition( x, y ){
+        this.position.set(x, y);
+    }
+
+    setVelocity( x, y ){
+        this.velocity.set(x, y);
+    }
+
+    update( dt ){
+        let dv = this.velocity.multiply( new Vector2(dt, dt) );
+        let da = this.acceleration.multiply( new Vector2(dt, dt) );
+        this.position = this.position.add( dv );
+        this.velocity = this.velocity.add( da );
+
+    }
+
+    render( ctx ){
+        ctx.save();
+        ctx.fillRect( this.position.x, this.position.y, 10, 10 );
+        ctx.restore();
+    }
+}
