2a873a2c14cdfe544b4b94f6845f7d5d50977097
Aniket kumar <ani1998ket@gmail.com>
2021-07-15 22:28:26 +0530

    Adds InputManager
    Input Manager can be asked for keyboard key presses

--- examples/MyScene.js
+++ examples/MyScene.js
@@ -1,6 +1,7 @@
 import Scene from "../src/Scene.js";
 import GameObject from "../src/GameObject.js";
 import { Vector2 } from "../src/Math.js";
+import InputManager from "../src/InputManager.js";
 
 const GRAVITY = new Vector2( 0, 100 )
 
@@ -9,15 +10,24 @@ export default class MyScene extends Scene{
     super()
     this.player = new GameObject();
     this.player.setPosition(10, 10);
-    this.player.setVelocity(100, 0);
     this.player.acceleration = GRAVITY;
   }
   update(dt){
-    this.player.update( dt )
-    if( this.player.position.y > 140 && this.player.velocity.y > 0 ){
-      this.player.position.y = 140;
-      this.player.velocity.y *= -0.90;
+    
+    this.player.velocity.x = 0;
+
+    if( InputManager.getKeyPress("ArrowRight") ||  InputManager.getKeyPress("d")){
+      this.player.velocity.x += 100;
     }
+    if( InputManager.getKeyPress("ArrowLeft")  ||  InputManager.getKeyPress("a")){
+      this.player.velocity.x -= 100;
+    }
+
+    this.player.update( dt )
+
+    if( this.player.position.y > 140 )
+      this.player.position.y = 140;
+
     this.player.position.x %= 300;
   }
   render(ctx){
---
+++ src/InputManager.js
@@ -0,0 +1,29 @@
+class InputManager{
+    
+    constructor(){
+        this.keyPress = {}
+    }
+
+    getKeyPress( key ){
+        if( this.keyPress[key] == undefined ){
+            this.keyPress[key] = false;
+        }
+
+        return this.keyPress[key];
+    }
+
+    setKeyPress( key, value ){
+        this.keyPress[key] = value;
+    }
+}
+
+let inputManager = new InputManager();
+window.addEventListener('keydown', (e)=>{
+    inputManager.setKeyPress(e.key, true);
+})
+
+window.addEventListener('keyup', (e)=>{
+    inputManager.setKeyPress(e.key, false);
+})
+
+export default inputManager;
